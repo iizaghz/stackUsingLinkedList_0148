98afeaef43bbb86d0eb690bc998f4726edd53c4b
iizaghz <sukmahawaiza@gmail.com>
2025-06-05 15:34:26 +0700

    add case 2 pop the topmost element from the stack

--- stack.cpp
+++ stack.cpp
@@ -96,7 +96,11 @@ int main()
             cin >> value;
             stack.push(value); //push the entered value onto the stack
             break;
-        
+        case 2:
+            stack.pop(); // pop the topmost element from the stack
+            break;
+        }
+
     }
     return 0;
 }
