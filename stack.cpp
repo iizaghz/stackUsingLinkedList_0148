80f543cbabfc968792918a28877ec20ab022386f
iizaghz <sukmahawaiza@gmail.com>
2025-06-05 15:16:42 +0700

    push operation : insert an element onto the top of the stack

--- stack.cpp
+++ stack.cpp
@@ -23,4 +23,8 @@ class Stack
     {
         top = NULL; // Initialize the stack with a null top pointer
     }
+
+    //push operation : insert an element onto the top of the stack
+    int push(int value)
+    {}
 };
