2c61ec33274d6dabf987a70fb9e3c18eab422380
iizaghz <sukmahawaiza@gmail.com>
2025-06-05 15:19:52 +0700

    add 4. update the top pointer to the new node

--- stack.cpp
+++ stack.cpp
@@ -30,5 +30,8 @@ class Stack
         Node *newNode = new Node();     // 1. allocate memory for the node
         newNode->data = value;          // 2. assign value
         newNode->next = top;            // 3. set the next pointer of the new node to the current
+        top = newNode;                  // 4. update the top pointer to the new node
+        cout << "Push value: " << value << endl;
+        return value;
     }
 };
